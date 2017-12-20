acab6016e6e5e6e36d843938e954aa53343f107a
Roy Cui <yaoyu.cui@siso.co.nz>
2017-12-20 18:00:32 +1300

    ADDED: basic views for levels, setting popup

---
+++ SmashMatch/Controllers/LevelsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LevelsViewController.swift
+//  SmashMatch
+//
+//  Created by Yaoyu Cui on 20/12/17.
+//  Copyright © 2017 Cameron Porter. All rights reserved.
+//
+
+import UIKit
+
+class LevelsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("this page is alive...")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
