36c5e90c44f2abd6c0e51e25d5b3dea61963dc68
Cameron Porter <cameron.porter@siso.co.nz>
2017-12-22 09:16:39 +1300

    Classic levels background

--- SmashMatch/OtherAppScreens/LevelsViewController.swift
+++ SmashMatch/OtherAppScreens/LevelsViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class LevelsViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var backgroundImage: UIImageView!
     
     override var prefersStatusBarHidden: Bool {
         return true
@@ -18,24 +20,16 @@ class LevelsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        backgroundImage.frame = scrollView.bounds
+//        scrollView.contentSize = backgroundImage.frame.size
+//        scrollView.minimumZoomScale = 0.4
+//        scrollView.maximumZoomScale = 4.0
+//        scrollView.setZoomScale(scrollView.minimumZoomScale, animated: false)
         print("this page is alive...")
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
