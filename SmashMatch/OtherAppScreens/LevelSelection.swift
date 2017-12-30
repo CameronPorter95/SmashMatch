92729e5de81301073355220c736d453687e11688
Cameron <cameronporter95@gmail.com>
2017-12-30 16:03:54 +1300

    Added background to new level selection scene

--- SmashMatch/OtherAppScreens/LevelSelection.swift
+++ SmashMatch/OtherAppScreens/LevelSelection.swift
@@ -16,15 +16,32 @@ class LevelSelection: SKScene {
     
     override func didMove(to view: SKView) {
         scrollView = SwiftySKScrollView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height), moveableNode: moveableNode, direction: .vertical)
-        let contentSize = CGSize(width: scrollView!.frame.width, height: scrollView!.frame.height * 15) // makes it 3 times the height
+        let contentSize = CGSize(width: scrollView!.frame.width, height: scrollView!.frame.height * 4.73089) // makes it 4.73089 times the height, image height is 4.73089 * screen height
         scrollView?.contentSize = contentSize
         view.addSubview(scrollView)
         addChild(moveableNode)
         
-        let backgroundImage = SKTexture(imageNamed: "levelsBG") //TODO Split into multiple images
-        let background = SKSpriteNode(texture: backgroundImage, size: contentSize)
-        background.position = CGPoint(x: 0, y: -contentSize.height/2)
-        moveableNode.addChild(background)
+        let backgroundImageSectionSize = CGSize(width: scrollView.frame.size.width, height:  scrollView.frame.size.height*1.182723) //4.73089/4
+        let midY = frame.midY - (frame.height*0.0913615) //0.182723/2
+        let backgroundImage1 = SKTexture(imageNamed: "LevelsBG1")
+        let background1 = SKSpriteNode(texture: backgroundImage1, size: backgroundImageSectionSize)
+        background1.position = CGPoint(x: frame.midX, y: midY)
+        moveableNode.addChild(background1)
+        
+        let backgroundImage2 = SKTexture(imageNamed: "LevelsBG2")
+        let background2 = SKSpriteNode(texture: backgroundImage2, size: backgroundImageSectionSize)
+        background2.position = CGPoint(x: frame.midX, y: midY - (backgroundImageSectionSize.height))
+        moveableNode.addChild(background2)
+        
+        let backgroundImage3 = SKTexture(imageNamed: "LevelsBG3")
+        let background3 = SKSpriteNode(texture: backgroundImage3, size: backgroundImageSectionSize)
+        background3.position = CGPoint(x: frame.midX, y: midY - (backgroundImageSectionSize.height * 2))
+        moveableNode.addChild(background3)
+        
+        let backgroundImage4 = SKTexture(imageNamed: "LevelsBG4")
+        let background4 = SKSpriteNode(texture: backgroundImage4, size: backgroundImageSectionSize)
+        background4.position = CGPoint(x: frame.midX, y: midY - (backgroundImageSectionSize.height * 3))
+        moveableNode.addChild(background4)
     }
     
     override func willMove(from view: SKView) {
